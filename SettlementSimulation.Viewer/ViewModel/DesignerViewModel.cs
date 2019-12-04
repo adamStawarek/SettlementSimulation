2402b1de58a819f968e75407464e901767153803
adam stawarek <adam.stawarek@op.pl>
2019-12-04 13:52:58 +0100

    designer view model fix adding breakpoints

--- SettlementSimulation.Viewer/ViewModel/DesignerViewModel.cs
+++ SettlementSimulation.Viewer/ViewModel/DesignerViewModel.cs
@@ -1,15 +1,18 @@
-﻿using OxyPlot;
+﻿using GalaSoft.MvvmLight.Command;
+using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using GalaSoft.MvvmLight.Command;
 
 namespace SettlementSimulation.Viewer.ViewModel
 {
     public class DesignerViewModel : INotifyPropertyChanged
     {
+        #region fields
+        private const int MaxGenerations = 10000;
+        #endregion
+
         #region properties
         private int _endY;
         public int EndY
@@ -20,7 +23,7 @@ namespace SettlementSimulation.Viewer.ViewModel
                 if (value == _endY) return;
                 _endY = value;
                 OnPropertyChanged();
-                SetTailPoints(_endY, false);
+                SetTailPoint(_endY);
             }
         }
         public PlotModel Plot { get; }
@@ -28,7 +31,7 @@ namespace SettlementSimulation.Viewer.ViewModel
         #endregion
 
         #region commands
-        public RelayCommand ClearBreakpointsCommand { get; } 
+        public RelayCommand ClearBreakpointsCommand { get; }
         #endregion
 
         public DesignerViewModel()
@@ -36,7 +39,7 @@ namespace SettlementSimulation.Viewer.ViewModel
             Plot = new PlotModel { Title = "Select breakpoints" };
             S1 = new LineSeries
             {
-                Points = { new DataPoint(1, 0), new DataPoint(10000, 9999) },
+                Points = { new DataPoint(1, 0), new DataPoint(MaxGenerations, MaxGenerations - 1) },
                 MarkerFill = OxyColors.SteelBlue,
                 MarkerType = MarkerType.Circle,
                 MarkerSize = 5
@@ -44,8 +47,8 @@ namespace SettlementSimulation.Viewer.ViewModel
 
             _endY = 9999;
             Plot.Series.Add(S1);
-            Plot.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Minimum = 0, Maximum = 10000 });
-            Plot.Axes.Add(new LinearAxis() { Position = AxisPosition.Bottom, Minimum = 0, Maximum = 10000 });
+            Plot.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Minimum = 0, Maximum = MaxGenerations });
+            Plot.Axes.Add(new LinearAxis() { Position = AxisPosition.Bottom, Minimum = 0, Maximum = MaxGenerations });
 
             Plot.MouseDown += (s, e) =>
             {
@@ -53,44 +56,40 @@ namespace SettlementSimulation.Viewer.ViewModel
                 if (series != null)
                 {
                     var point = S1.InverseTransform(e.Position);
-                    S1.Points.Add(new DataPoint(point.X, point.Y));
+                    var x = point.X;
+                    var y = (_endY / (double)MaxGenerations) * point.X;
+                    S1.Points.Add(new DataPoint(x, y));
                     S1.Points.Sort((p, p2) => p.X.CompareTo(p2.X));
                 }
 
                 Plot.InvalidatePlot(true);
             };
 
-            ClearBreakpointsCommand=new RelayCommand(ClearBreakpoints);
+            ClearBreakpointsCommand = new RelayCommand(ClearBreakpoints);
         }
 
         private void ClearBreakpoints()
         {
-            int numberOfPointsToRemove = S1.Points.Count - 2;//we leave first and last point
+            int numberOfPointsToRemove = S1.Points.Count - 2;
             S1.Points.RemoveRange(1, numberOfPointsToRemove);
             Plot.InvalidatePlot(true);
         }
 
-        public void SetTailPoints(int val, bool isFirstPoint)
+        public void SetTailPoint(int val)
         {
-            if (isFirstPoint)
-            {
-                S1.Points.RemoveAt(0);
-                S1.Points.Add(new DataPoint(0, val));
-            }
-            else
-            {
-                var lastIndex = S1.Points.Count - 1;
-                S1.Points.RemoveAt(lastIndex);
-                S1.Points.Add(new DataPoint(10000, val));
-            }
+            ClearBreakpoints();
+
+            var lastIndex = S1.Points.Count - 1;
+            S1.Points.RemoveAt(lastIndex);
+
+            S1.Points.Add(new DataPoint(MaxGenerations, val));
+
             S1.Points.Sort((p, p2) => p.X.CompareTo(p2.X));
             Plot.InvalidatePlot(true);
         }
 
         #region property changed
         public event PropertyChangedEventHandler PropertyChanged;
-
-
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
