5c81e36eab652f62c65a14e780af8c8a876e9b5d
adam stawarek <adam.stawarek@op.pl>
2020-01-07 14:44:08 +0100

    add building direction tests

--- SettlementSimulation.Engine.Tests.Unit/BuildingTests.cs
+++ SettlementSimulation.Engine.Tests.Unit/BuildingTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
+using SettlementSimulation.AreaGenerator.Models;
 using SettlementSimulation.Engine.Enumerators;
+using SettlementSimulation.Engine.Models;
 using SettlementSimulation.Engine.Models.Buildings;
 using SettlementSimulation.Engine.Models.Buildings.FirstType;
 using SettlementSimulation.Engine.Models.Buildings.SecondType;
@@ -18,32 +20,32 @@ namespace SettlementSimulation.Engine.Tests.Unit
         }
 
         [Test]
-        [Retry(30)]
-        public void At_First_Epoch_GetRandomGene_Generates_At_Least_One_Tavern_In_30_Tries()
+        [Retry(1000)]
+        public void At_First_Epoch_GetRandomGene_Generates_At_Least_One_Tavern_In_1000_Tries()
         {
             var gene = Building.GetRandom(Epoch.First);
             Assert.IsInstanceOf<Tavern>(gene);
         }
 
         [Test]
-        [Retry(100)]
-        public void At_Second_Epoch_GetRandomGene_Generates_At_Least_One_School_In_100_Tries()
+        [Retry(1000)]
+        public void At_Second_Epoch_GetRandomGene_Generates_At_Least_One_School_In_1000_Tries()
         {
             var gene = Building.GetRandom(Epoch.Second);
             Assert.IsInstanceOf<School>(gene);
         }
 
         [Test]
-        [Retry(200)]
-        public void At_Second_Epoch_GetRandomGene_Generates_At_Least_One_Church_In_200_Tries()
+        [Retry(1000)]
+        public void At_Second_Epoch_GetRandomGene_Generates_At_Least_One_Church_In_1000_Tries()
         {
             var gene = Building.GetRandom(Epoch.Second);
             Assert.IsInstanceOf<Church>(gene);
         }
 
         [Test]
-        [Retry(200)]
-        public void At_Second_Epoch_GetRandomGene_Generates_At_Least_One_AdministrationOffice_In_200_Tries()
+        [Retry(2000)]
+        public void At_Second_Epoch_GetRandomGene_Generates_At_Least_One_AdministrationOffice_In_2000_Tries()
         {
             var gene = Building.GetRandom(Epoch.Second);
             Assert.IsInstanceOf<Administration>(gene);
@@ -63,6 +65,78 @@ namespace SettlementSimulation.Engine.Tests.Unit
         {
             var gene = Building.GetRandom(Epoch.Third);
             Assert.IsInstanceOf<Port>(gene);
+        }
+
+        [Test]
+        public void When_Building_Is_Under_The_Horizontal_Road_Its_Direction_Is_Set_To_Down()
+        {
+            var road = new Road(new[]
+            {
+                new Point(0,1),
+                new Point(1,1)
+            });
+            var building = new Residence()
+            {
+                Position = new Point(0, 0)
+            };
+
+            road.AddBuilding(building);
+
+            Assert.AreEqual(Direction.Down, building.Direction);
+        }
+
+        [Test]
+        public void When_Building_Is_Above_The_Horizontal_Road_Its_Direction_Is_Set_To_Up()
+        {
+            var road = new Road(new[]
+            {
+                new Point(0,1),
+                new Point(1,1)
+            });
+            var building = new Residence()
+            {
+                Position = new Point(0, 2)
+            };
+
+            road.AddBuilding(building);
+
+            Assert.AreEqual(Direction.Up, building.Direction);
+        }
+
+        [Test]
+        public void When_Building_Is_On_The_Left_Side_The_Vertical_Road_Its_Direction_Is_Set_To_Left()
+        {
+            var road = new Road(new[]
+            {
+                new Point(1,0),
+                new Point(1,1)
+            });
+            var building = new Residence()
+            {
+                Position = new Point(0, 0)
+            };
+
+            road.AddBuilding(building);
+
+            Assert.AreEqual(Direction.Left, building.Direction);
+        }
+
+        [Test]
+        public void When_Building_Is_On_The_Right_Side_The_Vertical_Road_Its_Direction_Is_Set_To_Right()
+        {
+            var road = new Road(new[]
+            {
+                new Point(1,0),
+                new Point(1,1)
+            });
+            var building = new Residence()
+            {
+                Position = new Point(2, 0)
+            };
+
+            road.AddBuilding(building);
+
+            Assert.AreEqual(Direction.Right, building.Direction);
         }
     }
 }
